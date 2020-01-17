2d680009e5b201ebc5c76da067a9b3db1a9002a3
Jensen Koch <pdpdrums12@Jensens-MacBook-Pro.local>
2020-01-17 14:31:25 -0600

    initial commit

---
+++ recipes/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react'; 
+import './App.scss';
+import {Route} from 'react-router-dom';  
+
+function App() {
+  return (
+    <div className="App">
+      <p>Recipe App</p>
+    </div>
+  );
+}
+
+export default App;
